1f7c26cc5a09f266a0c9b2f93f6adbfec8ce6cf7
Gul Bulbul <79763247+gbulbul@users.noreply.github.com>
2024-07-18 12:57:31 -0400

    Update finding common submotif.py

--- finding common submotif.py
+++ finding common submotif.py
@@ -1,10 +1,3 @@
-d={}
-d['Rosalind1']='GATTACA'
-d['Rosalind2']='TAGACCA'
-d['Rosalind3']='ATACA'
-values_list=list(d.values())
-pairs=[(a, b) for idx, a in enumerate(values_list) for b in values_list[idx + 1:]]
-print(pairs)
 class CommonMotif:
       def FindCommonMotif(rosalind1, rosalind2):
          if len(rosalind1)<=len(rosalind2):
@@ -17,42 +10,42 @@ class CommonMotif:
          common_motif = []
          output = ""
          length = 0
-         
          for i in range(0, len(list1)):
-
               if list1[i] in list2:
                  common_motif.append(list1[i])
-
                  for j in range(i +1, len(list1)):
-
                      if ''.join(list1[i:j]) in rosalind2:
-                        common_motif.append(''.join(list1[i:j]))
-                        
-                        for k in range(j+1, len(list1)):
-                           
+                        common_motif.append(''.join(list1[i:j]))    
+                        for k in range(j+1, len(list1)):             
                             if list1[j:k] in list2:
-                                common_motif.append(list1[j:k])
-                                
+                                common_motif.append(list1[j:k])                        
                                 for m in range(k+1, len(list1)):
                                     if ''.join(list1[k:m]) in rosalind2:
                                        common_motif.append(''.join(list1[k:m]))
          for string in common_motif:
-
                 if length < len(list(string)):
                    length = len(list(string))
                    output = string
          return output
-import pandas as pd 
-d_pair_to_common={}
-for pair in pairs:
-    for i in range(0, len(list(pair[0]))-2):
-        d_pair_to_common[pair]= ''.join(CommonMotif.FindCommonMotif(pair[0].replace(pair[0][:i],''),pair[1]))
-        #print(d_pair_to_common)
-        df = pd.DataFrame.from_dict(d_pair_to_common,orient='index')
-        #print(df)
-        if len(df.index)==3:
-           arr = df[0].to_numpy()
-          
-           if (arr[0] == arr).all()==True:
-               print((arr[0]))
+if __name__=="__main__":
+    d={}
+    d['Rosalind1']='GATTACA'
+    d['Rosalind2']='TAGACCA'
+    d['Rosalind3']='ATACA'
+    values_list=list(d.values())
+    pairs=[(a, b) for idx, a in enumerate(values_list) for b in values_list[idx + 1:]]
+    print(pairs)
+    import pandas as pd 
+    d_pair_to_common={}
+    for pair in pairs:
+        for i in range(0, len(list(pair[0]))-2):
+            d_pair_to_common[pair]= ''.join(CommonMotif.FindCommonMotif(pair[0].replace(pair[0][:i],''),pair[1]))
+            #print(d_pair_to_common)
+            df = pd.DataFrame.from_dict(d_pair_to_common,orient='index')
+            #print(df)
+            if len(df.index)==3:
+               arr = df[0].to_numpy()
+              
+               if (arr[0] == arr).all()==True:
+                   print((arr[0]))
         
